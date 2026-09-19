1948f7fa3507850e172740558a0c8ab1043c9ad7
ewigl <ewiglichtl@gmail.com>
2026-09-19 10:35:36 +0800

    feat: update version to 0.1.0 in plugin and project files

--- ValheimChineseFixes.cs
+++ ValheimChineseFixes.cs
@@ -11,7 +11,7 @@ namespace ValheimChineseFixes
     {
         public const string PluginGUID = "com.ewigl.ValheimChineseFixes";
         public const string PluginName = "Valheim Chinese Fixes";
-        public const string PluginVersion = "0.0.1";
+        public const string PluginVersion = "0.1.0";
         private CustomLocalization localization;
         private const string langCode = "Chinese";
 
